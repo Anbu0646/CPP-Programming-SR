5271dff901a4e8364058c336cc35a1803cff44e6
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-02-16 20:43:20 +0530

    Create Minimum Distance Between Words [AMAZON] (Id-2614).cpp

---
+++ Minimum Distance Between Words [AMAZON] (Id-2614).cpp
@@ -0,0 +1,25 @@
+/*
+A string S is passed as the input. Two words W1 and W2 which are present in the string S are also passed as the input. The program must find the minimum distance D between W1 and W2 in S (in forward or reverse order) and print D as the output. 
+
+Input Format:  The first line will contain S. 
+               The second line will contain W1. 
+               The third line will contain W2. 
+Output Format: The first line will contain D - the minimum distance between W1 and W2 in S. 
+
+Boundary Conditions: Length of S is from 5 to 200. 
+
+Example Input/Output 1: 
+
+Input:  the brown quick frog quick the the quick 
+Output: 1 
+
+Explanation: quick and the are adjacent as the last two words. Hence distance between them is 1. 
+
+Example Input/Output 2: 
+
+Input:  the quick the brown quick brown the frog quick frog 
+Output: 3
+
+SOLUTION:
+*/
+
