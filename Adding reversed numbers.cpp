52f3c8386e9d69fdd6f9e0d7a193ec5343eef3b7
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-02-20 21:23:40 +0530

    Create Adding reversed numbers.cpp

---
+++ Adding reversed numbers.cpp
@@ -0,0 +1,54 @@
+/*
+A pair of numbers (X and Y) will be passed as input.
+The program must reverse the numbers and find the sum S.
+Then the sum S must be reversed and printed as output. 
+- If any leading zeroes are obtained while reversing any of the numerical values they should be discarded. 
+
+Boundary Conditions:  0 < X < 10000 
+                      0 < Y < 10000 
+
+Input Format:  First line will contain the value of X Second line will contain the value of Y
+Output Format: The first line will contain the sum S Sample
+
+Input/Output: 
+
+Example 1: 
+
+Input:  24 
+        1 
+Output: 34 
+
+Explanation: 24 when reversed is 42. So 42+1 = 43. When 43 is reversed it is 34 and hence 34 is the output. 
+
+Example 2: 
+
+Input:  305 
+        794 
+Output: 1 
+
+Explanation: 305 and 794 when reversed are 503 and 497. 503+497 = 1000. 1000 when reversed is 1 which is printed as output.
+
+SOLUTION:
+*/
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int rev(int n)
+{
+    int s=0;
+    while(n>0)
+    {
+        s=(s*10)+(n%10);
+        n/=10;
+    }
+    return s;
+}
+
+int main(int argc, char** argv)
+{
+    int x, y, z, a, b;
+    cin>>x>>y;
+    cout<<rev(rev(x)+rev(y));
+}
